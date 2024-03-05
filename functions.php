f18a41a8e22a30c6ba4a3132e5a848d15908e609
nicksleap <nicksleap@gmail.com>
2024-03-05 18:57:27 +0400

    Update functions.php
    create property post type

--- functions.php
+++ functions.php
@@ -92,3 +92,68 @@ function understrap_child_customize_controls_js() {
 	);
 }
 add_action( 'customize_controls_enqueue_scripts', 'understrap_child_customize_controls_js' );
+
+
+if ( ! function_exists('create_property_post_type') ) {
+
+	// Register Custom Post Type
+	function create_property_post_type() {
+
+		$labels = array(
+			'name'                  => _x( 'Properties', 'Post Type General Name', 'text_domain' ),
+			'singular_name'         => _x( 'Property', 'Post Type Singular Name', 'text_domain' ),
+			'menu_name'             => __( 'Properties', 'text_domain' ),
+			'name_admin_bar'        => __( 'Property', 'text_domain' ),
+			'archives'              => __( 'Item Archives', 'text_domain' ),
+			'attributes'            => __( 'Item Attributes', 'text_domain' ),
+			'parent_item_colon'     => __( 'Parent Item:', 'text_domain' ),
+			'all_items'             => __( 'All Items', 'text_domain' ),
+			'add_new_item'          => __( 'Add New Item', 'text_domain' ),
+			'add_new'               => __( 'Add New', 'text_domain' ),
+			'new_item'              => __( 'New Item', 'text_domain' ),
+			'edit_item'             => __( 'Edit Item', 'text_domain' ),
+			'update_item'           => __( 'Update Item', 'text_domain' ),
+			'view_item'             => __( 'View Item', 'text_domain' ),
+			'view_items'            => __( 'View Items', 'text_domain' ),
+			'search_items'          => __( 'Search Item', 'text_domain' ),
+			'not_found'             => __( 'Not found', 'text_domain' ),
+			'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+			'featured_image'        => __( 'Featured Image', 'text_domain' ),
+			'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
+			'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
+			'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
+			'insert_into_item'      => __( 'Insert into item', 'text_domain' ),
+			'uploaded_to_this_item' => __( 'Uploaded to this item', 'text_domain' ),
+			'items_list'            => __( 'Items list', 'text_domain' ),
+			'items_list_navigation' => __( 'Items list navigation', 'text_domain' ),
+			'filter_items_list'     => __( 'Filter items list', 'text_domain' ),
+		);
+		$args = array(
+			'label'                 => __( 'Property', 'text_domain' ),
+			'description'           => __( 'Properties information page.', 'text_domain' ),
+			'labels'                => $labels,
+			'supports'              => array( 'title', 'editor', 'thumbnail', 'comments', 'revisions', 'page-attributes' ),
+			'taxonomies'            => array( 'region', 'city' ),
+			'hierarchical'          => false,
+			'public'                => true,
+			'show_ui'               => true,
+			'show_in_menu'          => true,
+			'menu_position'         => 5,
+			'menu_icon'             => 'building',
+			'show_in_admin_bar'     => true,
+			'show_in_nav_menus'     => true,
+			'can_export'            => true,
+			'has_archive'           => true,
+			'exclude_from_search'   => false,
+			'publicly_queryable'    => true,
+			'capability_type'       => 'page',
+		);
+		register_post_type( 'property', $args );
+
+	}
+	add_action( 'init', 'create_property_post_type', 0 );
+	
+}
+
+
+
