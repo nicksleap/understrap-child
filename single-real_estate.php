164d9aff6c2fa9f6c6a6e8892ffee0da6c49d2e3
nicksleap <nicksleap@gmail.com>
2024-03-06 14:02:12 +0400

    Update single-real_estate.php
    Change html markdown

--- single-real_estate.php
+++ single-real_estate.php
@@ -15,63 +15,53 @@ $container = get_theme_mod( 'understrap_container_type' );
 <div class="wrapper" id="single-wrapper">
 
 	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+    <?php 
+      printf('<h1>%s</h1>', get_the_title());
 
-		<div class="row">
+      if (function_exists( 'get_field' )):
+        $photos = get_field('photos');
+        if (!empty($photos) && is_array($photos)) {
+          echo '<div class="galery">';
+          foreach($photos as $photo) {
+            printf(
+              '<div class="galery-item">%s</div>',
+              wp_get_attachment_image( $photo['ID'], 'large', false, [
+                'loading' => 'lazy'
+              ] )
+            );
+          }
+          echo '</div>';
+        }
 
-			<main class="site-main" id="main">
+        $area = get_field( 'area' );
+        if (!empty($area)) {
+          printf('<p>%s : <b>%sm<sup>2</sup></b></p>', __('Area'), $area);
+        }
 
-        <?php 
-          printf('<h1>%s</h1>', get_the_title());
+        $price = get_field('price');
+        if (!empty($price)) {
+          printf('<p>%s : <b>%s</b></p>', __('Price'), $price);
+        }
 
-          if (function_exists( 'get_field' )):
-            $photos = get_field('photos');
-            if (!empty($photos) && is_array($photos)) {
-              echo '<div class="galery">';
-              foreach($photos as $photo) {
-                printf(
-                  '<div class="galery-item">%s</div>',
-                  wp_get_attachment_image( $photo['ID'], 'large', false, [
-                    'loading' => 'lazy'
-                  ] )
-                );
-              }
-              echo '</div>';
-            }
+        $address = get_field('address');
+        if (!empty($address)) {
+          printf('<p>%s : <b>%s</b></p>', __('Adress'), $address);
+        }
 
-            $area = get_field( 'area' );
-            if (!empty($area)) {
-              printf('<p>%s : <b>%sm<sup>2</sup></b></p>', __('Area'), $area);
-            }
+        $living_area = get_field('living_area');
+        if (!empty($living_area)) {
+          printf('<p>%s : <b>%sm<sup>2</sup></b></p>', __('Living area'), $living_area);
+        }
 
-            $price = get_field('price');
-            if (!empty($price)) {
-              printf('<p>%s : <b>%s</b></p>', __('Price'), $price);
-            }
-
-            $address = get_field('address');
-            if (!empty($address)) {
-              printf('<p>%s : <b>%s</b></p>', __('Adress'), $address);
-            }
-
-            $living_area = get_field('living_area');
-            if (!empty($living_area)) {
-              printf('<p>%s : <b>%sm<sup>2</sup></b></p>', __('Living area'), $living_area);
-            }
-
-            $floor = get_field('floor');
-            if (!empty($floor)) {
-              printf('<p>%s : <b>%s</b></p>', __('Floor'), $floor);
-            }
+        $floor = get_field('floor');
+        if (!empty($floor)) {
+          printf('<p>%s : <b>%s</b></p>', __('Floor'), $floor);
+        }
 
 
 
-          endif;
-        ?>
-
-			</main>
-
-		</div><!-- .row -->
-
+      endif;
+    ?>
 	</div><!-- #content -->
 
 </div><!-- #single-wrapper -->
