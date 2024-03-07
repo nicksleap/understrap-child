793813b81c74977eef6917863bf4c3e0f0117e57
nicksleap <nicksleap@gmail.com>
2024-03-07 21:07:44 +0400

    Update custom-javascript.js

--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -1,1 +1,47 @@
 // Add your custom JS here.
+async function postData(url, formData) {
+  const response = await fetch( url, {
+    method: 'POST',
+    body: formData
+  });
+  return await response.json();
+};
+
+
+function submitFrom() {
+  const forms = document.querySelectorAll('form.ns_form');
+  console.log(forms)
+
+  if (forms.length === 0) return false;
+
+  forms.forEach(function(form) {
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+      // можно например тут сделать форму не доступной
+      const formData = new FormData(form);
+    
+      formData.append('nonce_code', ns_ajax.nonce);
+  
+      postData(ns_ajax.url, formData)
+        .then((response) => {
+          if (response.success !== true) {
+            console.log(response);
+          } else {
+            console.log(response);
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+        })
+        .finally(() => {
+          // можно например форму сделать снова доступной
+        });
+    });
+  })
+}
+
+
+submitFrom();
+console.log('here');
+
+
